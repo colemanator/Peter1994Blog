60bbbf35065c15a1c37e59da27e984d916b63749
Peter Coleman <peter@creativecuriosity.com.au>
2016-04-26 08:01:41 +1000

    build process for less

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,23 @@
 var gulp = require('gulp');
 var babel = require("gulp-babel");
 var uglify = require('gulp-uglify');
+var LessPluginCleanCSS = require('less-plugin-clean-css');
 var less = require('gulp-less');
 
-gulp.task("js-transpile", function () {
+gulp.task("javascript", function () {
     return gulp.src("./public/js/controllers/**/*.js")
         .pipe(babel())
-        .pipe(gulp.dest("./public/js/transpiled"));
-});
-
-gulp.task('js-compression', function() {
-    return gulp.src('./public/js/transpiled/**/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('./public/dist/js'));
 });
 
-gulp.task('default', ['js-transpile','js-compression']);
+gulp.task("less", function () {
+    var cleancss = new LessPluginCleanCSS({ advanced: true });
+    return gulp.src("./public/less/**/*.less")
+        .pipe(less({
+            plugins: [cleancss]
+        }))
+        .pipe(gulp.dest('./public/dist/css'));
+});
+
+gulp.task('default', ['javascript','less']);
